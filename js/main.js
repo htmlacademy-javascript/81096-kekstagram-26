d012639d4e6c7f49d35a4347a3660ecb11214ccc
dmitriiseliverstov <selikd@gmail.com>
2022-06-17 16:30:39 +0100

    Встроенные объекты и функции

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 
 function randomInteger(min, max) {
   // получить случайное число от (min-0) до (max+100)
-  const random = min - 0 + Math.random() * (max - min + 1);
+  const random = min + Math.random() * (max - min);
   return Math.round(random);
 }
-randomInteger(0, 100);
+// randomInteger(0, 100);
 
 
 // Функция для проверки максимальной длины строки
@@ -18,3 +18,50 @@ function checkMaxLeghtString (string, maxLengthSumbols = 140) {
   }
 }
 checkMaxLeghtString('');
+
+
+const Names = [
+  'Pedro',
+  'Fatima',
+  'Nuno',
+  'Alessandra',
+  'Ivan',
+  'Fedor',
+];
+
+const Messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+];
+
+const createObject = (Ids) => ({
+  id: Ids,
+  imageUrl: 'photos/' + Ids + '.jpg',
+  photoDescription: 'Мне понравилась фотография, потому что она  (четко передает чувства, эмоции, атмосферу.',
+  numberLike: (randomInteger(15, 200))
+});
+
+const createObjects = (count) => {
+  const objects = [];
+  for (let i = 1; i <= count; i++) {
+    objects.push(createObject(i));
+  }
+  return objects;
+}
+
+const maxObjectCount = 25;
+const objects = createObjects(maxObjectCount);
+
+// Cписок комментариев, оставленных другими пользователями к этой фотографии
+const createComments = () => {
+  return {
+    id: randomInteger(0, 10000),
+    avatar: 'img/avatar-' + randomInteger(0, 6) + '.svg',
+    massage: Messages[randomInteger(0, Messages.length - 1)],
+    name: Names[randomInteger(0, Names.length - 1)],
+   };
+};
+const similarComments = Array.from({length: 6}, createComments);
